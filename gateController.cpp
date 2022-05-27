86d2970b89d3c0861b22add1aa1018b4fea276f2
mpiatyko <piatikop@gmail.com>
2022-05-28 00:51:05 +0300

    GateController: refactoring, add functions.

--- gateController.cpp
+++ gateController.cpp
@@ -4,13 +4,18 @@
 
 gateController::gateController() {
     wiringPiSetupGpio();
-    pinMode(0, OUTPUT);
-    pinMode(1, OUTPUT);
-    pinMode(21, INPUT);
-    pullUpDnControl(21, PUD_UP);
-    digitalWrite(0, HIGH);
-    digitalWrite(1, HIGH);
-    wiringPiISR(21, INT_EDGE_BOTH, intrptHndlr);
+    pinMode(forwardPIN, OUTPUT);
+    pinMode(reversePIN, OUTPUT);
+    MotorControl(MotorMode::OFF);
+    pinMode(irSensorPIN, INPUT);
+    pinMode(closeSensorPIN, INPUT);
+    pinMode(openSensorPIN, INPUT);
+    pullUpDnControl(irSensorPIN, PUD_UP);
+    pullUpDnControl(closeSensorPIN, PUD_UP);
+    pullUpDnControl(openSensorPIN, PUD_UP);
+    wiringPiISR(irSensorPIN, INT_EDGE_BOTH, IrSensorIntrpt);
+    wiringPiISR(closeSensorPIN, INT_EDGE_BOTH, CloseSensorIntrpt);
+    wiringPiISR(openSensorPIN, INT_EDGE_BOTH, OpenSensorIntrpt);
 }
 
 gateController& gateController::getController() {
@@ -19,18 +24,31 @@ gateController& gateController::getController() {
     return controller;
 }
 
-void gateController::openGate(){
-    digitalWrite(0, HIGH);
-    digitalWrite(1, HIGH);
-    digitalWrite(1, LOW);
+void gateController::openGate() {
+    MotorControl(MotorMode::FORWARD);
 }
 
-void gateController::closeGate(){
-    digitalWrite(0, HIGH);
-    digitalWrite(1, HIGH);
-    digitalWrite(0, LOW);
+void gateController::closeGate() {
+    MotorControl(MotorMode::REVERSE);
 }
 
-void gateController::intrptHndlr(){
-    printf("Interupt registered PIN21 state: %d\n", digitalRead(21));
+void gateController::IrSensorIntrpt() {
+    printf("%s: %d\n", __func__, digitalRead(irSensorPIN));
+}
+
+void gateController::CloseSensorIntrpt() {
+    printf("%s: %d\n", __func__, digitalRead(closeSensorPIN));
+}
+
+void gateController::OpenSensorIntrpt() {
+    printf("%s: %d\n", __func__, digitalRead(openSensorPIN));
+}
+
+void gateController::MotorControl(const MotorMode mode) {
+    const u_MotorMode _mode(mode);
+
+    digitalWrite(forwardPIN, HIGH);
+    digitalWrite(reversePIN, HIGH);
+    digitalWrite(forwardPIN, _mode._forward);
+    digitalWrite(reversePIN, _mode._reverse);
 }
--- includes/gateController.h
+++ includes/gateController.h
@@ -1,12 +1,48 @@
+#include <cstdint>
+
+enum class State : uint8_t {
+    CLOSED,
+    OPENING,
+    OPENED,
+    CLOSING,
+    ERRROR,
+};
+
+enum class MotorMode : uint8_t {
+    OFF = 0u,
+    FORWARD,
+    REVERSE
+};
+
+union u_MotorMode {
+    const MotorMode _mode;
+    struct {
+        const uint8_t _forward : 1;
+        const uint8_t _reverse : 1;
+    };
+    u_MotorMode(const MotorMode mode) : _mode(mode) {}
+};
+
 class gateController {
    private:
+    static constexpr int forwardPIN = 1;
+    static constexpr int reversePIN = 0;
+    static constexpr int irSensorPIN = 21;
+    static constexpr int closeSensorPIN = 5;
+    static constexpr int openSensorPIN = 27;
+
+    State mState;
+
     gateController(const gateController&) = delete;
     gateController& operator=(gateController&) = delete;
     gateController();
+    void MotorControl(const MotorMode mode);
 
    public:
     static gateController& getController();
-    static void intrptHndlr();
+    static void IrSensorIntrpt();
+    static void CloseSensorIntrpt();
+    static void OpenSensorIntrpt();
     void openGate();
     void closeGate();
 
